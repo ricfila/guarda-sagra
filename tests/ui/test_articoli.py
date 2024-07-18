16dfccab74fa43bdff7f161b94c80a6ecfeb2da7
Giovanni <fornasier.giovanni909@gmail.com>
2024-07-18 12:23:10 +0200

    Modifiche ai test

--- tests/ui/test_articoli.py
+++ tests/ui/test_articoli.py
@@ -9,21 +9,18 @@ from src.ui.articoli import add_to_treeview, add_tipologia
 class TestArticoliModule(unittest.TestCase):
     @patch('tkinter.ttk.Treeview')
     @patch('src.ui.articoli.api_post')
-    def test_add_to_treeview(self, mock_refresh_treeview, mock_api_post):
+    def test_add_to_treeview(self, treeview_mock, mock_api_post):
         mock_api_post.return_value = 201
-        treeview_mock = ttk.Treeview(tk.Tk())
 
         add_to_treeview(treeview_mock, 'articoli_treeview', (('nome', 'Nuovo Articolo'), ('prezzo', '10.00')))
 
         mock_api_post.assert_called_once_with('/articoli', {'nome': 'Nuovo Articolo', 'prezzo': '10.00'})
 
-    @patch('tkinter.Tk')
+
     @patch('tkinter.ttk.Treeview')
     @patch('src.ui.articoli.api_post')
-    def test_add_tipologia(self, mock_refresh_treeview, mock_api_post):
+    def test_add_tipologia(self, treeview_mock, mock_api_post):
         mock_api_post.return_value = 201
-
-        treeview_mock = ttk.Treeview(tk.Tk())
 
         add_tipologia(treeview_mock,
                       (('nome', 'Nuova Tipologia'), ('posizione', '1'), ('sfondo', '#FFFFFF'), ('visibile', True)))
