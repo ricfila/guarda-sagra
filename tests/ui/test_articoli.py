e2d8a89515ba41888bbef54358f1b4706123f8a2
Giovanni <fornasier.giovanni909@gmail.com>
2024-07-18 12:20:52 +0200

    Modifiche ai test

--- tests/ui/test_articoli.py
+++ tests/ui/test_articoli.py
@@ -7,7 +7,7 @@ from src.ui.articoli import add_to_treeview, add_tipologia
 
 
 class TestArticoliModule(unittest.TestCase):
-
+    @patch('tkinter.ttk.Treeview')
     @patch('src.ui.articoli.api_post')
     def test_add_to_treeview(self, mock_refresh_treeview, mock_api_post):
         mock_api_post.return_value = 201
@@ -17,8 +17,8 @@ class TestArticoliModule(unittest.TestCase):
 
         mock_api_post.assert_called_once_with('/articoli', {'nome': 'Nuovo Articolo', 'prezzo': '10.00'})
 
-    @Mock.patch('tkinter.Tk')
-    @Mock.patch('tkinter.ttk.Treeview')
+    @patch('tkinter.Tk')
+    @patch('tkinter.ttk.Treeview')
     @patch('src.ui.articoli.api_post')
     def test_add_tipologia(self, mock_refresh_treeview, mock_api_post):
         mock_api_post.return_value = 201
