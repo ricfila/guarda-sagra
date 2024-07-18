d24a923fba3261fb84765314b948916d8e890351
Giovanni <fornasier.giovanni909@gmail.com>
2024-07-18 12:27:09 +0200

    test: magicmock invece di mock

--- tests/ui/test_articoli.py
+++ tests/ui/test_articoli.py
@@ -1,26 +1,23 @@
 import unittest
-from unittest.mock import patch, MagicMock, Mock
-import tkinter as tk
-from tkinter import ttk
+from unittest.mock import patch, MagicMock
 from src.ui.articoli import add_to_treeview, add_tipologia
 
-
-
 class TestArticoliModule(unittest.TestCase):
-    @patch('tkinter.ttk.Treeview')
     @patch('src.ui.articoli.api_post')
-    def test_add_to_treeview(self, treeview_mock, mock_api_post):
-        mock_api_post.return_value = 201
+    def test_add_to_treeview(self, mock_api_post):
+        mock_api_post.return_value = MagicMock(status_code=201)
+
+        treeview_mock = MagicMock(name='treeview_mock')
 
         add_to_treeview(treeview_mock, 'articoli_treeview', (('nome', 'Nuovo Articolo'), ('prezzo', '10.00')))
 
         mock_api_post.assert_called_once_with('/articoli', {'nome': 'Nuovo Articolo', 'prezzo': '10.00'})
 
-
-    @patch('tkinter.ttk.Treeview')
     @patch('src.ui.articoli.api_post')
-    def test_add_tipologia(self, treeview_mock, mock_api_post):
-        mock_api_post.return_value = 201
+    def test_add_tipologia(self, mock_api_post):
+        mock_api_post.return_value = MagicMock(status_code=201)
+
+        treeview_mock = MagicMock(name='treeview_mock')
 
         add_tipologia(treeview_mock,
                       (('nome', 'Nuova Tipologia'), ('posizione', '1'), ('sfondo', '#FFFFFF'), ('visibile', True)))
@@ -29,8 +26,5 @@ class TestArticoliModule(unittest.TestCase):
                                               {'nome': 'Nuova Tipologia', 'posizione': '1', 'sfondo': '#FFFFFF',
                                                'visibile': True})
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
